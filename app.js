e88919de7168dff295849fd235ea41e111920b8f
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-05-14 10:21:32 -0400

    level-up exercise 18

--- app.js
+++ app.js
@@ -171,8 +171,8 @@ game.catchPokemon = (pokemonObj) => {
     game.items[1].quantity--
 }
 
-game.catchPokemon(pokemon[150])
-console.log(game.items)
+// game.catchPokemon(pokemon[150])
+// console.log(game.items)
 
 /*
 Exercise 13
@@ -269,3 +269,39 @@ Solve Exercise 17 here:
 */
 
 console.log(game)
+
+/*
+Exercise 18
+Add a new property to the `game` object called `collection` and initialize its value to an empty array.
+
+Copy the `catchPokemon` method you wrote in Exercise Twelve and paste it below. Modify it so that:
+  - Ensure that no more than six Pokemon can be in the party at any time. 
+    Excess Pokemon should be placed in the `game.collection` array.
+  - It's up to you how to distribute Pokemon in a situation where more than six 
+    would be placed into the `game.party` array.
+
+Again, for this exercise, it's okay to have a negative number of pokeballs.
+
+After updating the method, use it by calling it and passing in a pokemon object of your choice from the `pokemon` data to catch it.
+
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 18 here:
+*/
+
+game.collection = []
+
+game.catchPokemon = (pokemonObj) => {
+    game.items[1].quantity--
+    if (game.party.length >= 6) {
+        game.collection.push(pokemonObj)
+
+    }
+    else {
+        game.party.push(pokemonObj)
+    }
+}
+
+game.catchPokemon(pokemon[37])
+// console.log(game.party)
+// console.log(game.party)
