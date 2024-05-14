3ead7f2d02efc59acdf6a12efd2125ee061182ce
andrewSantanastaso <andrewsantanastaso@gmail.com>
2024-05-14 16:10:44 -0400

    level up exercise 21

--- app.js
+++ app.js
@@ -377,5 +377,51 @@ game.catchPokemon = (pokeName) => {
 
 
 game.catchPokemon('mewtwo')
-
+console.log(game.items)
 console.log(game.collection)
+
+/*
+Exercise 21
+Dynamically construct an object with the existing `pokemon` data sorted by the different pokemon types. The object will have this structure:
+
+{
+  grass: [
+    { number: 1, name: 'Bulbasaur', type: 'grass', hp: 45, starter: true },
+    { number: 2, name: 'Ivysaur', type: 'grass', hp: 60, starter: false },
+    { number: 3, name: 'Venusaur', type: 'grass', hp: 80, starter: false },
+    * more grass type Pokemon objects...
+  ],
+  fire: [
+    { number: 4, name: 'Charmander', type: 'fire', hp: 39, starter: true },
+    * more fire type Pokemon objects...
+  ],
+  water: [
+    * water type Pokemon objects...
+  ],
+  * etc... until there is an array for every Pokemon type!
+}
+
+Log the object when it's constructed.
+
+Solve Exercise 21 here:
+*/
+
+
+
+const pokemonTypes = () => {
+    const output = {}
+
+    for (let i = 0; i < pokemon.length; i++) {
+        output[pokemon[i].type] = []
+        for (let j = 0; j < pokemon.length; j++) {
+            if (output[pokemon[j].type] === output[pokemon[i].type]) {
+                output[pokemon[i].type].push(pokemon[j])
+            }
+        }
+
+    }
+
+    return output
+}
+
+console.log(pokemonTypes())
